daf9f48a8cebe7289c74b4c58b1b847a9ab96360
Atharva Kamble <138187173+Athrudev@users.noreply.github.com>
2024-08-30 10:56:14 +0000

    setup ejs and cookie

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const express = require('express')
 const app = express()
+
+const path= require('path')
+const cookieParser = require('cookie-parser')
 const port = 3000
+
+
+app.set('view engine', 'ejs')
+app.use(express.json)
+app.use(express.urlencoded({extended: true}))
+app.use(express.static(path.join(__dirname,'public')))
+app.use(cookieParser())
 
 app.get('/', (req, res) => {
   res.send('Hello World!')

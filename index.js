659aec826bff0eb52a5a738ba6ecfa96f399e855
Atharva Kamble <138187173+Athrudev@users.noreply.github.com>
2024-08-30 11:13:52 +0000

    add index.ejs file

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(express.static(path.join(__dirname,'public')))
 app.use(cookieParser())
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.render('index')
 })
 
 app.listen(port, () => {
